a37d8d87d852846b5b420c3f92dfab581af00ed0
Rohit Nair <rohitnair987@gmail.com>
2016-09-02 04:21:00 -0400

    cleanup prints

--- Ass1/src/MazePathFinder.java
+++ Ass1/src/MazePathFinder.java
@@ -44,14 +44,14 @@ public class MazePathFinder {
 				System.out.println("NO");
 			}
 			else {
-
-//				for (int i = 0; i < maze.length; i++) {
-////					System.out.print(maze[i].length + " ");
-//					for (int j = 0; j < maze[0].length; j++) {
-//						System.out.print(maze[i][j]);
-//					}
-//					System.out.println();
-//				}
+				//display maze:
+				// for (int i = 0; i < maze.length; i++) {
+				// for (int j = 0; j < maze[0].length; j++) {
+				// System.out.print(maze[i][j]);
+				// }
+				// System.out.println();
+				// }
+				
 				// ********* Task 1 *********
 				if (task.equals("1")) {
 					System.out.println(isMazeLegal(maze) ? "YES" : "NO");
@@ -231,124 +231,63 @@ public class MazePathFinder {
 		char grid[][];
 		ArrayList<String> fileContents = new ArrayList<String>();
 
-		// try {
-		// if (!isLastCharNewLine(file)) {
-		// return null;
-		// }
-
 		int c = 0;
 		char line = 'z';
 		char last = 'y';
-		boolean cr = false;
-//		int i = 0, j = 0;
 		String inputLine = "";
 
-		// Scanner scanner = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while (true) {
 				c = br.read();
 				line = (char) c;
-				// System.out.print(line);
-				if (c != -1 && !isValid(line)) {
-					System.out.println("\nbhag: " + line + " saale");
+				
+				// Incorrect char in input such as ' ', d etc.
+				if (c != -1 && !isValid(line)) { 
 					return null;
 				}
-				// if (line == ' ') {// input 4 (extra space) handled
-				// System.out.println("\nspace");
-				// return null;
-				// }
-				if (line == '\n' && last == '\n') {// handles 3(2 crs)
-					System.out.println("\n2 returns together");
+				// Blank line
+				if (line == '\n' && last == '\n') {
 					return null;
 				}
-				// if (line == '\n' && !isValid(last)) {// input 4 (extra space)
-				// // handled
-				// System.out.println("\n2 returns");
-				// return null;
-				// }
+				// EOF reached - break
 				if (c == -1) {
-					System.out.println("\nbreaking....");
 					break;
 				}
 				last = line;
+
 				if (line != '\n') {
 					inputLine += line;
-					// grid[i][j++] = line;
 				}
 				else {
 					fileContents.add(inputLine);
-//					System.out.println(inputLine);
 					inputLine = "";
-					// i++;
 				}
 			}
-			System.out.println("eof char is " + c + " thanku");
-			// System.out.println("last char is " + last + " thanku");
 
-			if (last != '\n') {// input 2 (last line no cr) handled
-				System.out.println("last line doesnt ve cr");
+			if (last != '\n') { // if last line doesn've have a CR
 				return null;
 			}
-			else {
-				System.out.println("last line is ok");
-			}
-			System.out.println("dimen: " + fileContents.size() + " x " + fileContents.get(0).length());
+
 			grid = new char[fileContents.size()][fileContents.get(0).length()];
 			for (int i = 0; i < fileContents.size(); i++) {
 				grid[i] = fileContents.get(i).toCharArray();
 			}
-			// if(line == null) {
-			// System.out.println("invalid");
-			// return null;
-			// }
-			// System.out.println("line " + (c++) + " is " + line + " thanku");
 
 			return grid;
 		} catch (IOException e) {
+			System.out.println("Exception occured. Below is its Stack Trace:");
 			e.printStackTrace();
 		}
+		
 		return null;
-
-		// BufferedReader bIn = new BufferedReader(new FileReader(file));
-		// while ((line = bIn.readLine()) != null) {
-		// fileContents.add(line);
-		// }
-		// bIn.close();
-		//
-		// grid = new
-		// char[fileContents.size()][fileContents.get(0).toString().length()];
-		// for (int i = 0; i < fileContents.size(); i++) {
-		// grid[i] = fileContents.get(i).toString().toCharArray();
-		// }
-		// return grid;
-		// } catch (Exception e) {
-		// return null;
-		// }
 	}
 
-	private static boolean isValid(char last) {// handles 4(space) and 5(d)
-		if (last == '#' || last == '.' || last == 'S' || last == 'D' || last == '\n' || last == '?'||isNumber(last)) {
+	private static boolean isValid(char last) {
+		if (last == '#' || last == '.' || last == 'S' || last == 'D' || last == '\n' || last == '?' || isNumber(last)) {
 			return true;
 		}
 		return false;
 	}
 
-	private static boolean isLastCharNewLine(String file) {
-		int value = 0;
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			char c = ' ';
-			while ((value = br.read()) != -1) {
-				c = (char) value;
-			}
-			br.close();
-			if (c == '\n') {
-				return true;
-			}
-		} catch (Exception e) {
-			return false;
-		}
-		return false;
-	}
 }
