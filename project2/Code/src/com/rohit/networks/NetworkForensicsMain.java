68995fcc89d6e395e91cdd5c99d653b867c0255d
Rohit <rohitnair987@gmail.com>
2016-10-06 03:08:04 -0400

    tcp connections now stored as key=src.hashcode()+dest.hashcode() and value=packet number

--- project2/Code/src/com/rohit/networks/NetworkForensicsMain.java
+++ project2/Code/src/com/rohit/networks/NetworkForensicsMain.java
@@ -29,17 +29,18 @@ public class NetworkForensicsMain {
 		PCAPHeader pcapHeader;
 		PCAPData pcapData;
 		TCPConnectionTuple tcpConnectionTuple;
+		ArrayList<Integer> tcpConnectionKey;
 		Output out = new Output();
 
 		Map<Integer, PCAPData> allPCAPDataPackets = new HashMap<Integer, PCAPData>();
 		Map<String, Integer> tcpConnections = new HashMap<String, Integer>();
 		HashSet<String> tcpConnections4Tuple = new HashSet<String>();
+		HashMap<Integer, ArrayList<Integer>> tcpConnections2 = new HashMap<Integer, ArrayList<Integer>>();
 
 		int packetNumber = 1;
 
 		while (System.in.available() > 0) {
 			// while (packetNumber < 20) {
-			// System.out.println("Packet" + packetNumber++);
 			System.out.print("\n" + packetNumber + ": ");
 			pcapHeader = PCAPReader.readPCAPHeader();
 
@@ -47,31 +48,42 @@ public class NetworkForensicsMain {
 				System.out.println("\nNot a valid packet!");
 				System.exit(0);
 			}
-			// Utils.displayNextIBytes(pcapHeader.ActualLengthOfPacket);
-			// Utils.skipBytes(pcapHeader.ActualLengthOfPacket);
 
 			pcapData = PCAPReader.readPCAPData(pcapHeader.ActualLengthOfPacket);
 
 			allPCAPDataPackets.put(packetNumber, pcapData);
 
 			Utils.updateOutputValues(pcapData, out, taskNumber);
-			packetNumber++;
 
 			if (pcapData.ipHeader.TransportLayerProtocol == ConstantsEnum.TCP) {
 				tcpConnectionTuple = new TCPConnectionTuple();
-				System.out.println("ddd:"+pcapData.ipHeader.Source.hashCode());
-				
+
 				StringBuilder src = new StringBuilder();
 				src.append(pcapData.ipHeader.Source).append(":").append(pcapData.transportHeader.SourcePort);
 				StringBuilder dest = new StringBuilder();
-				src.append(pcapData.ipHeader.Destination).append(":").append(pcapData.transportHeader.DestinationPort);
+				dest.append(pcapData.ipHeader.Destination).append(":").append(pcapData.transportHeader.DestinationPort);
+
+				System.out.println("\nsrc :" + src);
+				System.out.println("dest:" + dest);
+
+				int key = src.toString().hashCode() + dest.toString().hashCode();
+				System.out.println("key :" + key + " = " + src.toString().hashCode() + " + " + dest.toString().hashCode());
+				
+				if (!tcpConnections2.containsKey(key)) {
+					tcpConnectionKey = new ArrayList<Integer>(packetNumber);
+					tcpConnections2.put(key, tcpConnectionKey);
+				} else {
+					tcpConnectionKey = tcpConnections2.get(key);
+					tcpConnectionKey.add(packetNumber);
+					tcpConnections2.replace(key, tcpConnectionKey);
+				}
 
 				String s1 = src.append(dest).toString();
 				String s2 = dest.append(src).toString();
 
 				// testing
-//				s1 = "\n" + s1;
-//				s2 = "\n" + s2;
+				// s1 = "\n" + s1;
+				// s2 = "\n" + s2;
 
 				tcpConnections4Tuple.add(s1);
 				tcpConnections4Tuple.add(s2);
@@ -84,10 +96,13 @@ public class NetworkForensicsMain {
 					tcpConnections.put(s2, 1);
 				}
 
+				packetNumber++;
 			}
 
-			System.out.println("\n" + pcapData.ipHeader.Source + " " + pcapData.transportHeader.SourcePort + " ");
-			System.out.println(pcapData.ipHeader.Destination + " " + pcapData.transportHeader.DestinationPort);
+			// System.out.println("\n" + pcapData.ipHeader.Source + " " +
+			// pcapData.transportHeader.SourcePort + " ");
+			// System.out.println(pcapData.ipHeader.Destination + " " +
+			// pcapData.transportHeader.DestinationPort);
 
 			// break;
 		}
@@ -95,12 +110,12 @@ public class NetworkForensicsMain {
 
 		if (taskNumber == 1) {
 			out.Task1.TotalPackets = packetNumber - 1;
-			System.out.println("asd" + tcpConnections.size());
-			System.out.println(tcpConnections);
-			
+			System.out.println("tcpConnections:" + tcpConnections.size());
+			// System.out.println(tcpConnections);
+			System.out.println("tcpConnections2:" + tcpConnections2.size());
 			out.Task1.TCPConnections = (int) Math.ceil(tcpConnections.size() / 2.0);
 		}
-		out.display(taskNumber);
+		// out.display(taskNumber);
 
 		// System.out.println("\nTime taken = " + (new Date().getTime() - d1) +
 		// "ms");
